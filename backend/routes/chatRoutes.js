e013273aeb823f4ab78eaf226b04867f051bdb3b
LawrenceDarko <darkolawrence@gmail.com>
2022-05-29 15:26:07 +0000

    clean

--- backend/routes/chatRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { protectRoute } = require('../middleware/auth');
-const { accessChat } = require('../controllers/chatControllers');
-
-const router = express.Router();
-
-router.route('/').post(protectRoute, accessChat);
-
-module.exports = router;
